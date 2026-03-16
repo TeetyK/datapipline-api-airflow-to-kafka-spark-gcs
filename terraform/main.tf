f60433425a2cf452d0832d5c644a27daaad2c4b2
TeetyK <teetysupergame7484@gmail.com>
2026-03-16 12:59:36 +0700

    test ci

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,30 @@
 #Terraform Google provider
-provider "google"{
-    project = var.project_id
-    region = var.region
-    # user_project_override = true
-    # billing_project = var.billing_id
-    zone = var.zone
-    credentials = file(var.service_account_path)
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  # user_project_override = true
+  # billing_project = var.billing_id
+  zone        = var.zone
+  credentials = file(var.service_account_path)
 }
 
 locals {
-    sa_key = jsondecode(file(var.service_account_path))
-    sa_email = local.sa_key.client_email
+  sa_key   = jsondecode(file(var.service_account_path))
+  sa_email = local.sa_key.client_email
 }
 
 #Terraform Google stronge bucket
-resource "google_storage_bucket" "api_gcs_bucket_name"{
-    name = var.api_gcs_bucket_name
-    location = var.location
-    force_destroy = true
+resource "google_storage_bucket" "api_gcs_bucket_name" {
+  name          = var.api_gcs_bucket_name
+  location      = var.location
+  force_destroy = true
 
-    # requester_pays = false
-    # uniform_bucket_level_access = true
+  # requester_pays = false
+  # uniform_bucket_level_access = true
 
-    # versioning {
-    #     enabled =false
-    # }
+  # versioning {
+  #     enabled =false
+  # }
 }
 
 # resource "google_storage_bucket_iam_member" "bucket_owner"{
@@ -35,37 +35,37 @@ resource "google_storage_bucket" "api_gcs_bucket_name"{
 
 # bronze layer
 resource "google_bigquery_dataset" "bronze" {
-    dataset_id = var.bigquery_bronze_dataset_name
-    description = "This's a bronze layer in BugQuery"
-    location = var.location
-    default_table_expiration_ms = 3600000
-    
-    # access {
-    #     role    ="WRITER"
-    #     user_by_email = local.sa_email
-    # }
+  dataset_id                  = var.bigquery_bronze_dataset_name
+  description                 = "This's a bronze layer in BugQuery"
+  location                    = var.location
+  default_table_expiration_ms = 3600000
+
+  # access {
+  #     role    ="WRITER"
+  #     user_by_email = local.sa_email
+  # }
 }
 # silver layer
 resource "google_bigquery_dataset" "silver" {
-    dataset_id = var.bigquery_silver_dataset_name
-    description = "This's a silver layer in BugQuery"
-    location = var.location
-    default_table_expiration_ms = 3600000
+  dataset_id                  = var.bigquery_silver_dataset_name
+  description                 = "This's a silver layer in BugQuery"
+  location                    = var.location
+  default_table_expiration_ms = 3600000
 
-    #  access {
-    #     role    ="WRITER"
-    #     user_by_email = local.sa_email
-    # }
+  #  access {
+  #     role    ="WRITER"
+  #     user_by_email = local.sa_email
+  # }
 }
 # gold layer
 resource "google_bigquery_dataset" "gold" {
-    dataset_id = var.bigquery_gold_dataset_name
-    description = "This's a gold layer in BugQuery"
-    location = var.location
-    default_table_expiration_ms = 3600000
+  dataset_id                  = var.bigquery_gold_dataset_name
+  description                 = "This's a gold layer in BugQuery"
+  location                    = var.location
+  default_table_expiration_ms = 3600000
 
-    #  access {
-    #     role    ="WRITER"
-    #     user_by_email = local.sa_email
-    # }
+  #  access {
+  #     role    ="WRITER"
+  #     user_by_email = local.sa_email
+  # }
 }
